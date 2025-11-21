e2104c5ddd7244771e26345f723f204cda1d78d4
ibarraja <javier.ibarra.mz@gmail.com>
2025-11-21 08:34:59 +0100

    mas ejercicios y ejercios 1 y 2 resueltos DOM

---
+++ IAW/Ejercicios_DOM_(manipular_HTML)/e2_contador/script_e2.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("btnSumar");
+const contador = document.getElementById("contador");
+
+btn.addEventListener("click", () => {
+    let valor = parseInt(contador.textContent);
+    contador.textContent = valor + 1;
+});
+
