1e4b5fcb420e2da8d169325d0a5c272f89d756b9
_-=Randomazzo=-_ <115214454+ibarraja@users.noreply.github.com>
2025-11-28 09:26:00 +0100

    Refactor DNI and password validation logic
    Updated validation logic for DNI and password. Improved regex patterns and added checks for special characters in the password.

--- IAW/jQuery/jQuery_2do_dia/ejercicio1_JS/main.js
+++ IAW/jQuery/jQuery_2do_dia/ejercicio1_JS/main.js
@@ -1,40 +1,59 @@
-let btn = document.getElementById("btn")
+let btn = document.getElementById("btn");
+let dni = document.getElementById("dni");
+let pass = document.getElementById("pass");
+let resultado = document.getElementById("resultado");
 
 btn.addEventListener("click", function() {
-    var dni = document.getElementById("dni").value;
-    var pass = document.getElementById("pass").value;
-    var resultado = document.getElementById("resultado");
-
-
     // Expresiones regulares
-    var formato1 = /^[0-9]{8}-[A-Za-z]{1}$/;
-    var formato2 = /^[0-9]{8}[A-Za-z]{1}$/;
-    var formato3 = /^[0-9]{2}\.[0-9]{3}\.[0-9]{3}-[A-Za-z]{1}$/;
-    var formatos = [formato1, formato2, formato3];
-
-    var formato_pass = /^[A-Za-z0-9]{8,10}$/;
+    //DNI
+    var formato1= /^[0-9]{8}[A-Za-z]{1}$/;
+    var formato2= /^[0-9]{2}\.[0-9]{3}\.[0-9]{3}-[A-Za-z]{1}$/;
+    var array_formatos_dni = [formato1, formato2];
+    
+    // Contraseña
+    var formato_pass = /^[!-z]{8,10}$/;
 
     // Validaciones
-    var dni_correcto = formatos.some(function(f) {
-        return f.test(dni);
-    });
+    let dni_correcto = false;
+    let pass_correcto = false;
 
-    var pass_correcto = formato_pass.test(pass);
+    for(var i=0; i<array_formatos_dni.length; i++) {
+        if(array_formatos_dni[i].test(dni.value)){
+            dni_correcto = true;
+        }
+    }
+
+    // validar si tiene un simbolo la contraseña 
+    if(formato_pass.test(pass.value)){
+        pass_correcto = true;
+    }
+    pass_tiene_caracter= false
+    for (var i=0; i<pass.length; i++) {
+        if (pass[i].in['+','*','@']){
+            pass_tiene_caracter = true;
+        }
+    }
+    var contraseña_super_segura = false;
+    if(pass_tiene_caracter && pass_correcto){
+        contraseña_super_segura=true;
+    }
+
 
     // Limpiar el div
     resultado.innerHTML = "";
-    resultado.classList.remove("correcto", "incorrecto");
+    resultado.classList.remove("correcto","incorrecto");
 
-    if (dni_correcto && pass_correcto) {
+    // Rellenar información en div
+    if (dni_correcto &&  contraseña_super_segura) {
         resultado.textContent = "Información validada correctamente!";
         resultado.classList.add("correcto");
-    } else {
+    } else{
         resultado.classList.add("incorrecto");
 
-        if (!dni_correcto) {
+        if(!dni_correcto) {
             resultado.innerHTML += "<p>DNI formato incorrecto!</p>";
         }
-        if (!pass_correcto) {
+        if(!contraseña_super_segura) {
             resultado.innerHTML += "<p>Contraseña formato incorrecto!</p>";
         }
     }
