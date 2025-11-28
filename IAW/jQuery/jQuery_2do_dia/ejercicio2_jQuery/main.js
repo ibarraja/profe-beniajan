4543f9507f018e0c0095bea4928553840b9e1eb1
_-=Randomazzo=-_ <115214454+ibarraja@users.noreply.github.com>
2025-11-28 09:26:27 +0100

    Refactor DNI and password validation logic

--- IAW/jQuery/jQuery_2do_dia/ejercicio2_jQuery/main.js
+++ IAW/jQuery/jQuery_2do_dia/ejercicio2_jQuery/main.js
@@ -1,45 +1,43 @@
 $(document).ready(function() {
-    $("#btn").click(function(event) {
-        event.preventDefault(); // evita recargar el formulario
+    $("#btn").click(function(){
 
         var dni = $("#dni").val();
         var pass = $("#pass").val();
         var $resultado = $("#resultado");
 
         // Expresiones regulares
-        var formato1 = /^[0-9]{8}-[A-Za-z]{1}$/;
-        var formato2 = /^[0-9]{8}[A-Za-z]{1}$/;
-        var formato3 = /^[0-9]{2}\.[0-9]{3}\.[0-9]{3}-[A-Za-z]{1}$/;
-        var formatos = [formato1, formato2, formato3];
-
-        var formato_pass = /^[A-Za-z0-9]{8,10}$/;
+        var formato1= /^[0-9]{8}[A-Za-z]{1}$/;
+        var formato2= /^[0-9]{2}\.[0-9]{3}\.[0-9]{3}-[A-Za-z]{1}$/;
+        var array_formatos_dni = [formato1, formato2];
+        
+        var formato_pass = /^[!-z]{8,10}$/;
 
         // Validaciones
         var dni_correcto = false;
-        for (var i = 0; i < formatos.length; i++) {
-            if (formatos[i].test(dni)) {
+        for (var i = 0;i < array_formatos_dni.length; i++) {
+            if(array_formatos_dni[i].test(dni)) {
                 dni_correcto = true;
-                break;
             }
         }
 
         var pass_correcto = formato_pass.test(pass);
 
-        // Limpiar el div
+        // Limpiamos div
         $resultado.empty().removeClass("correcto incorrecto");
 
-        if (dni_correcto && pass_correcto) {
+        if (dni_correcto &&  pass_correcto) {
             $resultado.text("Información validada correctamente!")
                       .addClass("correcto");
-        } else {
-            $resultado.addClass("incorrecto");
-            if (!dni_correcto) {
+            $resultado.classList.add("correcto");
+        } else{
+            $resultado.classList.addClass("incorrecto");
+
+            if(!dni_correcto) {
                 $resultado.append("<p>DNI formato incorrecto!</p>");
             }
-            if (!pass_correcto) {
+            if(!pass_correcto) {
                 $resultado.append("<p>Contraseña formato incorrecto!</p>");
             }
         }
-    });
-});
-
+    })
+})
